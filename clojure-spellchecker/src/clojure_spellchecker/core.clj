fefa37f44a7fa230749cc828d669387971126731
woodson delhia <woodsondelhia@hotmail.com>
2016-06-19 04:03:12 +0900

    Added file zipping function

--- clojure-spellchecker/src/clojure_spellchecker/core.clj
+++ clojure-spellchecker/src/clojure_spellchecker/core.clj
@@ -1,6 +1,7 @@
 (ns clojure-spellchecker.core
   (:gen-class)
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [clojure.java.shell :only [sh] :as shell])
   (:import (org.apache.commons.lang3 StringUtils)))
 
 (def words 
@@ -15,6 +16,15 @@
 
 (defn min-distance [word]
   (apply min-key (partial distance word) words))
+
+(defn pack [name & rest] (shell/sh "sh" "-c" (apply str " " " zip " " resources/" name " -r " (str/join " " rest) )  ))
+
+(defn pack-resources [in & rest]
+  (if (empty? rest)
+    (println (str "Please select a file to zip." (empty? rest) ))
+    (let [files rest]
+      (shell/sh "sh" "-c" (apply str " zip " " resources/" in " -r " (str/join " " files)))
+    )))
 
 (defn -main
   "Receive a word and check if its an english word."
